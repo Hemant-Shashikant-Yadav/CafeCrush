59403f23ff0a8273ae1896ca063fc1a675ef499b
Hemant Shashikant Yadav. <hemantsryadav@gmail.com>
2024-03-30 01:45:10 +0530

    basic structure

---
+++ server/routes/main.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+router.get('', (req, res) => {
+
+    const locals =
+    {
+        title: 'Home Page',
+        desciption: 'Welcome to the Home Page '
+    }
+    res.render('index', {locals});
+});
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
+// router.get('', (req, res) => {
+//     res.send('Hello World!');
+// });
+
+
+module.exports = router;

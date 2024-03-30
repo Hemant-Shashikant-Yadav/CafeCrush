4cd7405aaecff03d50f704b3770c4a00e040a0ec
Hemant Shashikant Yadav. <hemantsryadav@gmail.com>
2024-03-30 23:46:26 +0530

    databse cpnnection done successfully

---
+++ server/models/Post.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+
+const schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+});
+
+const Post = mongoose.model('Post', PostSchema);

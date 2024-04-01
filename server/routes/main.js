0b5315c5618053d72d57049b08689f1465e531ec
Hemant Shashikant Yadav. <hemantsryadav@gmail.com>
2024-04-01 22:29:07 +0530

    ss

--- server/routes/main.js
+++ server/routes/main.js
@@ -10,7 +10,7 @@ router.get('/', (req, res) => {
         title: 'Home Page',
         desciption: 'Welcome to the Home Page '
     }
-    res.render('index', {locals});
+    res.render('index', { locals });
 });
 router.get('/about', (req, res) => {
     res.render('about');
@@ -20,5 +20,17 @@ router.get('/about', (req, res) => {
 //     res.send('Hello World!');
 // });
 
+
+router.get('/test-db-connection', async (req, res) => {
+    try {
+        const client = await connectToMongoDB();
+        // Perform a simple query to test the connection
+        const db = client.db('CafeCrush'); // Replace 'yourDatabaseName' with your actual database name
+        const collections = await db.listCollections().toArray();
+        res.json({ message: 'Connected to MongoDB', collections });
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to connect to MongoDB', error: error.message });
+    }
+});
 
 module.exports = router;
